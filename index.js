c9215341df04197f3d6af61e8c5cafd77f2b5d72
RitoTorri <cortezfrancisco025@gmail.com>
2026-06-10 23:43:12 -0400

    desplegar es horrible

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 // index.js en la raíz
-const App = require('./src/app');
-
-// ¡AQUÍ ESTÁ EL ERROR! Debes usar el operador 'new'
-const appInstance = new App();
-
-// Exportas la propiedad 'app' de la instancia, que es el objeto Express
-module.exports = appInstance.app;
+const app = require('./src/app');
+module.exports = app;
--- src/app.js
+++ src/app.js
@@ -51,4 +51,4 @@ class App {
     }*/
 }
 
-module.exports = App;
+module.exports = new App().app;
